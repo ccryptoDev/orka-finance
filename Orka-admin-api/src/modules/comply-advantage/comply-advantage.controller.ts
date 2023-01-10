690747e1f02f727c442436ae79a545aec030c2fa
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-09 21:27:45 -0400

    advantage controller

--- Orka-admin-api/src/modules/comply-advantage/comply-advantage.controller.ts
+++ Orka-admin-api/src/modules/comply-advantage/comply-advantage.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, SetMetadata, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+
+import { RolesGuard } from '../../guards/roles.guard';
 import { ComplyAdvantageService } from './comply-advantage.service';
 
-@Controller('comply-advantage')
+@ApiTags('Middesk')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@Controller('loans/:loanId/comply-advantage')
 export class ComplyAdvantageController {
   constructor(private readonly complyAdvantageService: ComplyAdvantageService) {}
+
+  @ApiOperation({ summary: 'Find comply advantage report by loan ID' })
+  @Get()
+  public async findByLoanId(@Param('loanId') loanId: string): Promise<any> {
+    const middesk = await this.complyAdvantageService.findByLoanId(loanId);
+
+    return middesk;
+  }
 }
