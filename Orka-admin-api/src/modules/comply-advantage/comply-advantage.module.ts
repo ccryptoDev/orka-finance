e02f4c9fcffd2dde7ff8cd2381eb191f6d517218
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-09 21:27:58 -0400

    advantage module

--- Orka-admin-api/src/modules/comply-advantage/comply-advantage.module.ts
+++ Orka-admin-api/src/modules/comply-advantage/comply-advantage.module.ts
@@ -4,11 +4,12 @@ import { ComplyAdvantageController } from './comply-advantage.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ComplyAdvantageReportRepository } from 'src/repository/complyAdvantage.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerEntity } from '../../entities/customer.entity';
 
 @Module({
   controllers: [ComplyAdvantageController],
   providers: [ComplyAdvantageService],
-  imports: [TypeOrmModule.forFeature([ComplyAdvantageReportRepository]), HttpModule],
+  imports: [TypeOrmModule.forFeature([ComplyAdvantageReportRepository, CustomerEntity]), HttpModule],
   exports: [ComplyAdvantageService]
 })
 export class ComplyAdvantageModule { }
