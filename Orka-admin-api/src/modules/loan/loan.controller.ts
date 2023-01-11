e1b0ba11c49e6fe636d9f7a96fbca69a116c3f19
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-10 23:18:37 -0400

    loan module

--- Orka-admin-api/src/modules/loan/loan.controller.ts
+++ Orka-admin-api/src/modules/loan/loan.controller.ts
@@ -1,8 +1,5 @@
 import {
   Controller,
-  Get,
-  HttpStatus,
-  HttpCode,
   ParseUUIDPipe,
   Param,
   Post
@@ -13,59 +10,11 @@ import { LoanService } from './loan.service';
 
 @Controller('loan')
 export class LoanController {
-  constructor(private readonly loanService: LoanService) { }
+  constructor(private readonly loanService: LoanService) {}
 
-  @Get('pull-equifax-credit-report/:id')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: "Pull equifax credit report and save in DB" })
-  async equifaxCreditReport(
-    @Param('id', ParseUUIDPipe) id: string
-  ) {
-    return this.loanService.equifaxCreditPull(id)
-  }
-
-  @Get('equifax-set2-report/:id')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: "Pull equifax credit report and save in DB" })
-  async equifaxSet2Report(
-    @Param('id', ParseUUIDPipe) id: string
-  ) {
-    return this.loanService.equifaxCreditPullSet2(id)
-  }
-
-  @Post('middesk/:loanid')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: "Pull middesk report and save in DB" })
-  async getMiddeskId(
-    @Param('loanid', ParseUUIDPipe) loanid: string
-  ) {
-    return this.loanService.getMiddeskId(loanid);
-  }
-
-  @Get('middeskreport/:loanid')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: "Get middesk report from DB" })
-  async getMiddeskReport(
-    @Param('loanid', ParseUUIDPipe) loanid: string
-  ) {
-    return this.loanService.getMiddeskReport(loanid)
-  }
-
-  @Post('complyadvantagereport/:loanid')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: "Pull Comply Advantage report and save in DB and show" })
-  async getComplyAdvantageReport(
-    @Param('loanid', ParseUUIDPipe) loanid: string
-  ) {
-    return this.loanService.getComplyAdvantageReport(loanid)
-  }
-
-  @Get('reportResult/:loanid')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: "Get DE rules result" })
-  async getReports(
-    @Param('loanid', ParseUUIDPipe) loanid: string
-  ) {
-    return this.loanService.getReportsResult(loanid)
+  @Post(':id/decision')
+  @ApiOperation({ summary: 'Get the reports and generate the automated decision results' })
+  public async getDecisions(@Param('id', ParseUUIDPipe) id: string) {
+    return this.loanService.getDecisions(id);
   }
 }
