dccc119a72431ff60db093d166e182cf83793d8d
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-12 00:57:56 -0400

    loan service

--- Orka-admin-api/src/modules/loan/loan.service.ts
+++ Orka-admin-api/src/modules/loan/loan.service.ts
@@ -1,411 +1,72 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { getManager, Repository } from 'typeorm';
-import { HttpService } from '@nestjs/axios';
-import { Agent } from 'https';
-import { readFileSync } from 'fs';
 
-import { Creditreport, Flags } from '../../entities/creditReport.entity'
-import { CreditreportRepository } from './../../repository/creditRepositiry.repository';
-import { MiddeskCreationDTO, MiddeskService } from '../middesk/middesk.service';
+import { CustomerEntity } from 'src/entities/customer.entity';
+import { PlaidService } from '../plaid/plaid.service';
+import { CreditReportService } from '../credit-report/credit-report.service';
+import { ReportType } from '../../entities/creditReport.entity'
+import { MiddeskService } from '../middesk/middesk.service';
 import { ComplyAdvantageService } from '../comply-advantage/comply-advantage.service';
-import { CustomerEntity } from '../../entities/customer.entity';
-import { EquifaxService } from '../equifax/equifax.service';
-import { DecisionServiceService } from '../decision-service/decision-service.service';
+import { Decision, DecisionServiceService, RuleResult, Situation } from '../decision-service/decision-service.service';
+import { MailService } from '../../mail/mail.service';
 
 @Injectable()
 export class LoanService {
   constructor(
-    @InjectRepository(CreditreportRepository) private readonly creditreportRepository: CreditreportRepository,
     @InjectRepository(CustomerEntity) private readonly customerRepository: Repository<CustomerEntity>,
-    private equifaxService: EquifaxService,
-    private readonly Middesk: MiddeskService,
-    private readonly ComplyAdvantage: ComplyAdvantageService,
-    private readonly decisionService: DecisionServiceService
+    private readonly plaidService: PlaidService,
+    private readonly creditReportService: CreditReportService,
+    private readonly middeskService: MiddeskService,
+    private readonly complyAdvantageService: ComplyAdvantageService,
+    private readonly decisionService: DecisionServiceService,
+    private readonly mailService: MailService
   ) {}
-  
-  public async equifaxCreditPull(loanId: string) {
-    const existingCreditReport = await this.creditreportRepository.findOne({
-      loan_id: loanId,
-      reportType: 'equifax-consumer'
-    });
 
-    if (!existingCreditReport) {
-      const customer = await this.findCustomer(loanId);
-      let ownerStreet = '';
-      let ownerHouseNumber = '';
-  
-      if (customer.ownerAddress) {
-        ownerStreet = customer.ownerAddress.split(",")[0].trim(),
-        ownerHouseNumber = customer.ownerAddress.split(",")[0].split(" ")[0].trim()
-      }
-  
-      const report = await this.equifaxService.getConsumerReport({
-        firstName: customer.ownerFirstName,
-        lastName: customer.ownerLastName,
-        socialSecurityNumber: customer.ownerSSN,
-        birthDate: customer.ownerDOB,
-        city: customer.ownerCity,
-        state: customer.ownerState,
-        zipCode: customer.ownerZipCode,
-        houseNumber: ownerHouseNumber,
-        streetName: ownerStreet,
-        phone: customer.ownerPhone
-      });
-      const creditScore = report.consumers.equifaxUSConsumerCreditReport[0].models?.find((model) => model.type === 'FICO')?.score;
-      const creditReport = new Creditreport();
-  
-      creditReport.loan_id = loanId;
-      creditReport.reportType = 'equifax-consumer';
-      creditReport.report = JSON.stringify({
-        creditScore: creditScore ? Number(creditScore) : null,
-        equaifax: report,
-        success: true,
-        loanOffersResponse: null,
-        Rules: [] // Change when implementing the decision engine
-      });
-  
-      await this.creditreportRepository.save(creditReport);
-    }
-  }
-  
-  public async equifaxCreditPullSet2(loanId: string) {
-    const existingCreditReport = await this.creditreportRepository.findOne({
-      loan_id: loanId,
-      reportType: 'equifax-comercial-set2'
-    });
+  public async getDecisions(loanId: string): Promise<Decision> {
+    await this.plaidService.getAssets(loanId);
 
-    if (!existingCreditReport) {
-      const customer = await this.findCustomer(loanId);
-      const report = await this.equifaxService.getCommercialReport({
-        businessAddress: customer.businessAddress,
-        businessCity: customer.city,
-        businessLegalName: customer.legalName,
-        businessState: this.getState(customer.zipCode),
-        businessZipCode: customer.zipCode
-      });
-      const decisions = this.decisionService.getCommercialRuleDecisions(report);
-      const creditReport = new Creditreport();
-  
-      creditReport.loan_id = loanId;
-      creditReport.reportType = 'equifax-comercial-set2';
-      creditReport.report = JSON.stringify({ ...report, Rules: decisions });
-  
-      await this.creditreportRepository.save(creditReport);
-    }
-  }
-
-  public async getReports(loanId) {
-      let data: any = {};
-      data['equifaxReport'] = [];
-      let creditReports: any = await this.creditreportRepository.find({ loan_id: loanId, delete_flag: Flags.N });
-      for (let i = 0; i < creditReports.length; i++) {
-          data['equifaxReport'].push({
-              id: creditReports[i].id,
-              reportType: creditReports[i].reportType,
-              report: JSON.parse(creditReports[i]['report'])
-          });
-      }
-      return data;
-  }
-
-  async getReportsResult(loanId) {
-      let data: any = {};
-      data['equifax-consumer'] = null;
-      data['equifax-comercial-set2'] = null;
-      const creditReportsEq: any = await this.creditreportRepository.findOne({ loan_id: loanId, delete_flag: Flags.N, reportType: "equifax-consumer" });
-    
-      if(creditReportsEq && creditReportsEq.report)  {
-          const report = JSON.parse(creditReportsEq.report);
-          report.Rules.forEach(rule => {
-              if(data['equifax-consumer'] !== "BYPASS"){
-                  data['equifax-consumer'] = rule.RuleStatus
-              }
-          });
-      }
-
-
-      const creditReportCommercial: any = await this.creditreportRepository.findOne({ loan_id: loanId, delete_flag: Flags.N, reportType: "equifax-comercial-set2" });
-      
-      if(creditReportCommercial && creditReportCommercial.report) {
-          const reportComm = JSON.parse(creditReportCommercial.report);
-          const rulesCom = reportComm.EfxTransmit.Rules
-          rulesCom.forEach(rule => {
-              if(data['equifax-comercial-set2'] !== "BYPASS"){
-                  data['equifax-comercial-set2'] = rule.RuleStatus
-              }
-          });
-      } else {
-          data['equifax-comercial-set2'] = "PASS"
-      }
-
-
-      return data;
-  }
-
-  async getMiddeskId(loanId: string) {
-    const entityManager = getManager();
-    const [customer] = await entityManager.query(
-      `
-        SELECT
-          "legalName",
-          "taxId",
-          email,
-          "businessAddress",
-          city,
-          state,
-          "zipCode",
-          "businessPhone"
-        FROM
-          tblcustomer
-        WHERE
-          "loanId" = $1
-      `,
-      [loanId]
-    );
-
-    if (!customer) {
-      throw new NotFoundException({
-        statusCode: 404,
-        message: 'Customer not found'
-      });
-    }
-
-    if (!customer.legalName || !customer.businessAddress) {
-      throw new ConflictException({
-        statusCode: 409,
-        message: 'Customer legal name and address are required to generate middesk report'
-      });
-    }
-
-    const shortState = (customer.zipCode!=null)?this.getState(customer.zipCode.toString()) : '';
-    const middeskCreationDto: MiddeskCreationDTO = {
-      name: customer.legalName,
-      addresses: [{
-        address_line1: customer.businessAddress,
-        state: shortState,
-        city: customer.city,
-        postal_code: customer.zipCode
-      }],
-      tin: {
-        tin: customer.taxId
-      },
-      phone_numbers: [{
-        phone_number: customer.businessPhone
-      }]
-    };
-
-    await this.Middesk.create(middeskCreationDto, loanId);
-
-    return { statusCode: 200, customer };
-  }
-
-  async getMiddeskReport(loanid) {
-    const res = await this.Middesk.get(loanid);
-
-    return { statusCode: 200, data: res.data };
-  }
-
-  async getComplyAdvantageReport(loanid) {
-      const res = await this.ComplyAdvantage.getReport(loanid);
-
-      if (res.status == true) {
-          return { statusCode: 200, data: res.data }
-      }
-  }
-
-  private async findCustomer(loanId: string): Promise<CustomerEntity> {
     const customer = await this.customerRepository.findOne({ loanId });
+    const equifaxConsumerReport = await this.creditReportService.getConsumerReport(customer);
+    const equifaxCommercialReport = await this.creditReportService.getCommercialReport(customer);
+    const complyAdvantageReport = await this.complyAdvantageService.getReport(customer);
+    const middeskReport = await this.middeskService.getReport(customer);
+    const lendingLimit = await this.decisionService.setLendingLimitPersonalGuarantor(loanId, equifaxConsumerReport);
+    const decision = this.decisionService.getDecision({
+      equifaxConsumerReport,
+      equifaxCommercialReport,
+      complyAdvantageReport,
+      middeskReport,
+      lendingLimit
+    });
 
-    if (!customer) {
-      throw new NotFoundException({ statusCode: 404, message: 'Customer not found' });
-    }
+    await this.creditReportService.create({
+      loanId,
+      reportType: ReportType.Consumer,
+      report: equifaxConsumerReport,
+      ruleResults: this.filterRuleResults(decision.ruleResults, [1, 2, 3]) // Equifax Consumer rules
+    });
+    await this.creditReportService.create({
+      loanId,
+      reportType: ReportType.Commercial,
+      report: equifaxCommercialReport,
+      ruleResults: this.filterRuleResults(decision.ruleResults, [7, 8, 9, 10, 11]) // Equifax Commercial rules
+    });
+    await this.complyAdvantageService.create({
+      loanId,
+      report: complyAdvantageReport,
+      ruleResults: this.filterRuleResults(decision.ruleResults, [4, 5, 6]) // Comply Advantage rules
+    });
+    await this.middeskService.create({
+      loanId,
+      report: middeskReport,
+      ruleResults: this.filterRuleResults(decision.ruleResults, [12, 13, 14, 15, 16, 17]) // Middesk rules
+    });
 
-    return customer;
+    return decision;
   }
 
-  private getState(zipString) {
-    /* Ensure param is a string to prevent unpredictable parsing results */
-    if (typeof (zipString) !== 'string') {
-        console.log('Must pass the zipcode as a string.');
-        return;
-    }
-
-    /* Ensure we have exactly 5 characters to parse */
-    if (zipString.length !== 5) {
-        console.log('Must pass a 5-digit zipcode.');
-        return;
-    }
-
-    /* Ensure we don't parse strings starting with 0 as octal values */
-    const zipcode = parseInt(zipString, 10);
-
-    let st;
-    let state;
-
-    /* Code cases alphabetized by state */
-    if (zipcode >= 35000 && zipcode <= 36999) {
-        st = 'AL';
-        state = 'Alabama';
-    } else if (zipcode >= 99500 && zipcode <= 99999) {
-        st = 'AK';
-        state = 'Alaska';
-    } else if (zipcode >= 85000 && zipcode <= 86999) {
-        st = 'AZ';
-        state = 'Arizona';
-    } else if (zipcode >= 71600 && zipcode <= 72999) {
-        st = 'AR';
-        state = 'Arkansas';
-    } else if (zipcode >= 90000 && zipcode <= 96699) {
-        st = 'CA';
-        state = 'California';
-    } else if (zipcode >= 80000 && zipcode <= 81999) {
-        st = 'CO';
-        state = 'Colorado';
-    } else if ((zipcode >= 6000 && zipcode <= 6389) || (zipcode >= 6391 && zipcode <= 6999)) {
-        st = 'CT';
-        state = 'Connecticut';
-    } else if (zipcode >= 19700 && zipcode <= 19999) {
-        st = 'DE';
-        state = 'Delaware';
-    } else if (zipcode >= 32000 && zipcode <= 34999) {
-        st = 'FL';
-        state = 'Florida';
-    } else if ((zipcode >= 30000 && zipcode <= 31999) || (zipcode >= 39800 && zipcode <= 39999)) {
-        st = 'GA';
-        state = 'Georgia';
-    } else if (zipcode >= 96700 && zipcode <= 96999) {
-        st = 'HI';
-        state = 'Hawaii';
-    } else if (zipcode >= 83200 && zipcode <= 83999) {
-        st = 'ID';
-        state = 'Idaho';
-    } else if (zipcode >= 60000 && zipcode <= 62999) {
-        st = 'IL';
-        state = 'Illinois';
-    } else if (zipcode >= 46000 && zipcode <= 47999) {
-        st = 'IN';
-        state = 'Indiana';
-    } else if (zipcode >= 50000 && zipcode <= 52999) {
-        st = 'IA';
-        state = 'Iowa';
-    } else if (zipcode >= 66000 && zipcode <= 67999) {
-        st = 'KS';
-        state = 'Kansas';
-    } else if (zipcode >= 40000 && zipcode <= 42999) {
-        st = 'KY';
-        state = 'Kentucky';
-    } else if (zipcode >= 70000 && zipcode <= 71599) {
-        st = 'LA';
-        state = 'Louisiana';
-    } else if (zipcode >= 3900 && zipcode <= 4999) {
-        st = 'ME';
-        state = 'Maine';
-    } else if (zipcode >= 20600 && zipcode <= 21999) {
-        st = 'MD';
-        state = 'Maryland';
-    } else if ((zipcode >= 1000 && zipcode <= 2799) || (zipcode == 5501) || (zipcode == 5544)) {
-        st = 'MA';
-        state = 'Massachusetts';
-    } else if (zipcode >= 48000 && zipcode <= 49999) {
-        st = 'MI';
-        state = 'Michigan';
-    } else if (zipcode >= 55000 && zipcode <= 56899) {
-        st = 'MN';
-        state = 'Minnesota';
-    } else if (zipcode >= 38600 && zipcode <= 39999) {
-        st = 'MS';
-        state = 'Mississippi';
-    } else if (zipcode >= 63000 && zipcode <= 65999) {
-        st = 'MO';
-        state = 'Missouri';
-    } else if (zipcode >= 59000 && zipcode <= 59999) {
-        st = 'MT';
-        state = 'Montana';
-    } else if (zipcode >= 27000 && zipcode <= 28999) {
-        st = 'NC';
-        state = 'North Carolina';
-    } else if (zipcode >= 58000 && zipcode <= 58999) {
-        st = 'ND';
-        state = 'North Dakota';
-    } else if (zipcode >= 68000 && zipcode <= 69999) {
-        st = 'NE';
-        state = 'Nebraska';
-    } else if (zipcode >= 88900 && zipcode <= 89999) {
-        st = 'NV';
-        state = 'Nevada';
-    } else if (zipcode >= 3000 && zipcode <= 3899) {
-        st = 'NH';
-        state = 'New Hampshire';
-    } else if (zipcode >= 7000 && zipcode <= 8999) {
-        st = 'NJ';
-        state = 'New Jersey';
-    } else if (zipcode >= 87000 && zipcode <= 88499) {
-        st = 'NM';
-        state = 'New Mexico';
-    } else if ((zipcode >= 10000 && zipcode <= 14999) || (zipcode == 6390) || (zipcode == 501) || (zipcode == 544)) {
-        st = 'NY';
-        state = 'New York';
-    } else if (zipcode >= 43000 && zipcode <= 45999) {
-        st = 'OH';
-        state = 'Ohio';
-    } else if ((zipcode >= 73000 && zipcode <= 73199) || (zipcode >= 73400 && zipcode <= 74999)) {
-        st = 'OK';
-        state = 'Oklahoma';
-    } else if (zipcode >= 97000 && zipcode <= 97999) {
-        st = 'OR';
-        state = 'Oregon';
-    } else if (zipcode >= 15000 && zipcode <= 19699) {
-        st = 'PA';
-        state = 'Pennsylvania';
-    } else if (zipcode >= 300 && zipcode <= 999) {
-        st = 'PR';
-        state = 'Puerto Rico';
-    } else if (zipcode >= 2800 && zipcode <= 2999) {
-        st = 'RI';
-        state = 'Rhode Island';
-    } else if (zipcode >= 29000 && zipcode <= 29999) {
-        st = 'SC';
-        state = 'South Carolina';
-    } else if (zipcode >= 57000 && zipcode <= 57999) {
-        st = 'SD';
-        state = 'South Dakota';
-    } else if (zipcode >= 37000 && zipcode <= 38599) {
-        st = 'TN';
-        state = 'Tennessee';
-    } else if ((zipcode >= 75000 && zipcode <= 79999) || (zipcode >= 73301 && zipcode <= 73399) || (zipcode >= 88500 && zipcode <= 88599)) {
-        st = 'TX';
-        state = 'Texas';
-    } else if (zipcode >= 84000 && zipcode <= 84999) {
-        st = 'UT';
-        state = 'Utah';
-    } else if (zipcode >= 5000 && zipcode <= 5999) {
-        st = 'VT';
-        state = 'Vermont';
-    } else if ((zipcode >= 20100 && zipcode <= 20199) || (zipcode >= 22000 && zipcode <= 24699) || (zipcode == 20598)) {
-        st = 'VA';
-        state = 'Virginia';
-    } else if ((zipcode >= 20000 && zipcode <= 20099) || (zipcode >= 20200 && zipcode <= 20599) || (zipcode >= 56900 && zipcode <= 56999)) {
-        st = 'DC';
-        state = 'Washington DC';
-    } else if (zipcode >= 98000 && zipcode <= 99499) {
-        st = 'WA';
-        state = 'Washington';
-    } else if (zipcode >= 24700 && zipcode <= 26999) {
-        st = 'WV';
-        state = 'West Virginia';
-    } else if (zipcode >= 53000 && zipcode <= 54999) {
-        st = 'WI';
-        state = 'Wisconsin';
-    } else if (zipcode >= 82000 && zipcode <= 83199) {
-        st = 'WY';
-        state = 'Wyoming';
-    } else {
-        st = 'none';
-        state = 'none';
-        console.log('No state found matching', zipcode);
-    }
-
-    return st;
+  private filterRuleResults(ruleResults: RuleResult[], ruleIdsToBeFiltered: number[]): RuleResult[] {
+    return ruleResults.filter((ruleResult) => ruleIdsToBeFiltered.includes(ruleResult.RuleId));
   }
 }
