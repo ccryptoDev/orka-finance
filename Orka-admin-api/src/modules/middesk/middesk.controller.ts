8fcedc05f71935a437c5c72f3cf5fd9e365d34ae
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-12 00:58:40 -0400

    middesk module

--- Orka-admin-api/src/modules/middesk/middesk.controller.ts
+++ Orka-admin-api/src/modules/middesk/middesk.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, SetMetadata, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+
+import { RolesGuard } from '../../guards/roles.guard';
 import { MiddeskService } from './middesk.service';
 
-@Controller('middesk')
+export const Roles = (...roles: string[]) => SetMetadata('role', roles);
+
+@ApiTags('Middesk')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@Controller('loans/:loanId/middesk')
 export class MiddeskController {
   constructor(private readonly middeskService: MiddeskService) {}
+
+  @ApiOperation({ summary: 'Find middesk report by loan ID' })
+  @Get()
+  public async findByLoanId(@Param('loanId') loanId: string): Promise<any> {
+    const middesk = await this.middeskService.findByLoanId(loanId);
+
+    return middesk;
+  }
 }
