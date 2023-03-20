709d31c69c8ed3ccadb81bdc415ea3ee2f712ba5
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-03-20 03:42:08 -0400

    updated opportunity service

--- Orka-installer-api/src/modules/opportunity/opportunity.service.ts
+++ Orka-installer-api/src/modules/opportunity/opportunity.service.ts
@@ -10,7 +10,6 @@ import { Milestone } from '../../repository/milestone.repository';
 import { MilestoneEntity } from '../../entities/milestone.entity';
 import { ProductEntity } from '../../entities/products.entity';
 import { FinancingContractInvitationTemplateData, MailService } from '../mail/mail.service';
-import { LogsService } from 'src/common/log/logs.service';
 
 @Injectable()
 export class OpportunityService {
@@ -19,8 +18,7 @@ export class OpportunityService {
     @InjectRepository(CustomerRepository) private readonly customerRepository: CustomerRepository,
     @InjectRepository(LoanRepository) private readonly loanRepository: LoanRepository,
     @InjectRepository(ProductEntity) private readonly productRepository: Repository<ProductEntity>,
-    private readonly mailService: MailService,
-    private readonly logsService: LogsService,
+    private readonly mailService: MailService
   ) {}
 
   public async getLoanProductsDetails(id:string)
@@ -45,56 +43,254 @@ export class OpportunityService {
       }
   }
   
-  public  async getLoanDetails(id:string)
-    {
-        try {
-            const entityManager = getManager();
-            let customerFinanceDetails = await entityManager.query(`select t1.phase_flag ,t1.status_flag , t2."legalName", t2."ownerFirstName",t2."ownerLastName", t2.email,t2."city",t2."state",t2."zipCode", t2."businessPhone", t2."businessAddress", t1.phase_flag,  t1.status_flag, t1."financingRequested", t1."financingTermRequested",t2."loanProductID",t1.ref_no, t1."apr" from tblloan t1 join tblcustomer t2 on t1.id=t2."loanId" where t2."loanId"='${id}'`)
-            let siteProjectDetails = await entityManager.query(`select "city","state","zipCode","businessInstallAddress","businessInstallCity","businessInstallState","businessInstallZipCode","businessInstallLat","businessInstallLng","siteType","electricCompany","avgUtilPerMonth","avgConsumptionPerMonth","arraySize","panelManufacturer","inverterNamePlateCapacity","inverterManufacturer","batteryCapacity","batteryManufacturer", "mountType", "estGenerationRate","estGeneration","nonSolarEquipmentWork","nonSolarProjectCost","totalProjectCost" from tblcustomer where "loanId"='${id}'`)
-            let activity = await entityManager.query(`select CONCAT ('LOG_',t.id) as id, t.module as module, concat(t3.email,' - ',INITCAP(t4."name"::text)) as user, t."createdAt" as createdAt from tbllog t join tblloan t2 on t.loan_id = t2.id left join tbluser t3 on t3.id = t.user_id left join tblroles t4 on t4.id = t3."role" where t.loan_id='${id}'`)
-            let batteryManufaturer = await entityManager.query(`select value from tbldropdown where mainfield = 'batteryManufacturer'`);
-            let inverterManufacturer = await entityManager.query(`select value from tbldropdown where mainfield = 'inverterManufacturer'`);
-            let mountType = await entityManager.query(`select value from tbldropdown where mainfield = 'mountType'`);
-            let panelManufacturer = await entityManager.query(`select value from tbldropdown where mainfield = 'panelManufacturer'`)
-            let loanProducts = await entityManager.query(`select t2."productId", t2.name, t2."type" , t2."tenorMonths", t2."tenorYears", t2."interestBaseRate", t2."achDiscount", t2."achDiscountInterestRate", t2."dealerFee", t2."originationFee",t2.prepayment_flag, t2.prepayment, t2."prepaymentMonth", t2.downpayment, t2."mpfAchWItcPrepay",t2."mpfAchWOPrepay", t2."mpfCheckWItcPrepay", t2."mpfCheckWOPrepay"  from tblpartnerproduct t1 join tblproduct t2 on t1."productId"  = t2."productId" where t1."installerId" ='${id}' and t1.active_flag ='Y' and t2.status ='ACTIVE'`)
-            let contractReviewComments = await entityManager.query(`select comments, "updatedAt" from tblsalescontractreview WHERE loanid='${id}' AND status='needadditionalinfo'`)
-            let milestoneReviewComments = await entityManager.query(`select comments,milestone,status from tblmilestone WHERE loanid='${id}'`)
+  public  async getLoanDetails(id:string) {
+    try {
+      const entityManager = getManager();
+      const customerFinanceDetails = await entityManager.query(
+        `
+          select
+            t1.phase_flag,
+            t1.status_flag,
+            t2."legalName",
+            t2."ownerFirstName",
+            t2."ownerLastName",
+            t2.email,
+            t2."city",
+            t2."state",
+            t2."zipCode",
+            t2."businessPhone",
+            t2."businessAddress",
+            t1.phase_flag,
+            t1.status_flag,
+            t1."financingRequested",
+            t1."financingTermRequested",
+            t1."interestRate",
+            t2."loanProductID",
+            t1.ref_no,
+            t1."apr",
+            t1.approval_denial_date
+          from
+            tblloan t1
+          join
+            tblcustomer t2 on t1.id = t2."loanId"
+          where
+            t2."loanId" = $1
+        `,
+        [id]
+      );
+      const siteProjectDetails = await entityManager.query(
+        `
+          select
+            "city",
+            "state",
+            "zipCode",
+            "businessInstallAddress",
+            "businessInstallCity",
+            "businessInstallState",
+            "businessInstallZipCode",
+            "businessInstallLat",
+            "businessInstallLng",
+            "siteType",
+            "electricCompany",
+            "avgUtilPerMonth",
+            "avgConsumptionPerMonth",
+            "arraySize",
+            "panelManufacturer",
+            "inverterNamePlateCapacity",
+            "inverterManufacturer",
+            "batteryCapacity",
+            "batteryManufacturer",
+            "mountType",
+            "estGenerationRate",
+            "estGeneration",
+            "nonSolarEquipmentWork",
+            "nonSolarProjectCost",
+            "totalProjectCost"
+          from
+            tblcustomer
+          where "loanId" = $1
+        `,
+        [id]
+      );
+      const activity = await entityManager.query(
+        `
+          select
+            CONCAT ('LOG_',t.id) as id,
+            t.module as module,
+            concat(t3.email,
+            ' - ',
+            INITCAP(t4."name"::text)) as user,
+            t."createdAt" as createdAt
+          from
+            tbllog t
+          join
+            tblloan t2 on t.loan_id = t2.id
+          left join
+            tbluser t3 on t3.id = t.user_id
+          left join
+            tblroles t4 on t4.id = t3."role"
+          where
+            t.loan_id = $1
+        `,
+        [id]
+      );
+      const batteryManufaturer = await entityManager.query(
+        `
+          select
+            value
+          from
+            tbldropdown
+          where
+            mainfield = 'batteryManufacturer'
+        `
+      );
+      const inverterManufacturer = await entityManager.query(
+        `
+          select
+            value
+          from
+            tbldropdown
+          where
+            mainfield = 'inverterManufacturer'
+        `
+      );
+      const mountType = await entityManager.query(
+        `
+          select
+            value
+          from
+            tbldropdown
+          where
+            mainfield = 'mountType'
+        `
+      );
+      const panelManufacturer = await entityManager.query(
+        `
+          select
+            value
+          from
+            tbldropdown
+          where
+            mainfield = 'panelManufacturer'
+        `
+      );
+      const loanProducts = await entityManager.query(
+        `
+          select
+            t2."productId",
+            t2.name,
+            t2."type",
+            t2."tenorMonths",
+            t2."tenorYears",
+            t2."interestBaseRate",
+            t2."achDiscount",
+            t2."achDiscountInterestRate",
+            t2."dealerFee",
+            t2."originationFee",
+            t2.prepayment_flag,
+            t2.prepayment,
+            t2."prepaymentMonth",
+            t2.downpayment,
+            t2."mpfAchWItcPrepay",
+            t2."mpfAchWOPrepay",
+            t2."mpfCheckWItcPrepay",
+            t2."mpfCheckWOPrepay"
+          from
+            tblpartnerproduct t1
+          join
+            tblproduct t2 on t1."productId" = t2."productId"
+          where
+            t1."installerId" = $1 and t1.active_flag = 'Y' and t2.status = 'ACTIVE'
+        `,
+        [id]
+      );
+      const contractReviewComments = await entityManager.query(
+        `
+          select
+            comments,
+            "updatedAt"
+          from
+            tblsalescontractreview
+          WHERE
+            loanid = $1 AND status = 'needadditionalinfo'
+        `,
+        [id]
+      );
+      const milestoneReviewComments = await entityManager.query(
+        `
+          select
+            comments,
+            milestone,
+            status
+          from
+            tblmilestone
+          where 
+            loanid = $1
+        `,
+        [id]
+      );
+      const selectedProduct = await entityManager.query(
+        `
+          select
+            t2."productId",
+            t2.name,
+            t2."type",
+            t2."tenorMonths",
+            t2."tenorYears",
+            t2."interestBaseRate", 
+            t2."achDiscount",
+            t2."achDiscountInterestRate",
+            t2."dealerFee",
+            t2."originationFee",
+            t2.prepayment_flag,
+            t2.prepayment,
+            t2."prepaymentMonth",
+            t2.downpayment,
+            t2."mpfAchWItcPrepay",
+            t2."mpfAchWOPrepay",
+            t2."mpfCheckWItcPrepay", 
+            t2."mpfCheckWOPrepay",
+            lm."financingApproved"  
+          from
+            tblloan tln
+          join
+            tblproduct t2 on CAST(tln."productId"  as int) = t2."productId"
+          join
+            tbllendinglimit lm on lm."loanId" = tln.id AND lm."productId" = tln."productId"
+          where
+            tln.id = $1
+        `,
+        [id]
+      );
+      
+      return {
+        statusCode: 200,
+        customerFinanceDetails,
+        siteProjectDetails,
+        batteryManufaturer,
+        inverterManufacturer,
+        mountType,
+        panelManufacturer,
+        activity,
+        loanProducts,
+        contractReviewComments,
+        milestoneReviewComments,
+        selectedProduct
+      };
+    } catch (error) {
+      let resp = new InternalServerErrorException(error).getResponse();
 
-            let selectedProduct = await entityManager.query(`select t2."productId", t2.name, t2."type" , t2."tenorMonths", t2."tenorYears", t2."interestBaseRate", 
-            t2."achDiscount", t2."achDiscountInterestRate", t2."dealerFee", t2."originationFee",
-            t2.prepayment_flag, t2.prepayment, t2."prepaymentMonth", t2.downpayment,
-            t2."mpfAchWItcPrepay",t2."mpfAchWOPrepay", t2."mpfCheckWItcPrepay", 
-            t2."mpfCheckWOPrepay", lm."financingApproved"  
-            from tblloan tln join tblproduct t2 on CAST(tln."productId"  as int) = t2."productId"
-            join tbllendinglimit lm on lm."loanId"=tln.id AND lm."productId"=tln."productId" where tln.id='${id}' `)
-            
-            return {
-                statusCode:200,
-                customerFinanceDetails,
-                siteProjectDetails,
-                batteryManufaturer,
-                inverterManufacturer,
-                mountType,
-                panelManufacturer,
-                activity,
-                loanProducts,
-                contractReviewComments,
-                milestoneReviewComments,
-                selectedProduct
-            }
-        } catch (error) {
-            console.log(error)
-            let resp = new InternalServerErrorException(error).getResponse();
-            if (Object.keys(resp).includes('name'))
-                resp = Object.values(resp)[Object.keys(resp).indexOf('name')];
-            return {
-                statusCode: 500,
-                message: [resp],
-                error: 'Bad Request',
-            };
-        }
+      if (Object.keys(resp).includes('name')) {
+        resp = Object.values(resp)[Object.keys(resp).indexOf('name')];
+      }
+        
+      return {
+        statusCode: 500,
+        message: [resp],
+        error: 'Bad Request',
+      };
     }
-  
+  }
 
   public async putDetails(id: string, opportunityDto: OpportunityDto) {
     try {
